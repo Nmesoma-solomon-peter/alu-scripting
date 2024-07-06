d64cb6b0d99ed29ea0f168766fa04b31e47732cb
Nmesoma-solomon-peter <nmesomasolo2809@gmail.com>
2024-07-06 14:18:58 +0000

    added pep8

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,6 +7,7 @@ If the subreddit is invalid, the function returns 0.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
